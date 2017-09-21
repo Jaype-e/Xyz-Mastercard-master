ee55a98dccb0ae754a90120ef9d46aaba929f348
Jayprakash gupta <jayprakash.gupta.cse15@itbhu.ac.in>
2017-09-22 00:41:45 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Welcome</title>
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+		<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    </head>
+	<body bgcolor=#1A1A1A> 
+		<table  width="100%" >
+			<!--header-->
+			<tr id="header" >
+				<td id="header_icon" style="border:2px solid #C3073F;" width="10%">
+					<img src="icon1.jpg" height="140">
+				</td>
+				<td style="text-align:center;font:  50px impact;color:#C3073F;" width="70%">
+					<div> XYZ Mastercard</div>
+				</td>
+				<td width="10%"></td>
+				<td id="header_button" width="10%">
+					
+					<div style="text-align:center;font:  50px impact;color:#C3073F;">
+						LOGIN
+					</div>
+						
+					<div style="text-align:center;font:  50px impact;color:#C3073F;">
+						SIGNUP
+					</div>
+
+				</td>
+
+
+			</tr>
+			</table>
+			<table width="100%">
+			<!--headerbar-->
+				<tr id="headerbar">
+
+					<td id="headerbar_button">
+						<a id="headerbar_link" href="#"> <i class="fa fa-home"></i>&nbsp;HOME</a>
+					</td>
+					<td id="headerbar_button">
+						<a id="headerbar_link" href="#"><i class="fa fa-shield"></i>&nbsp;ABOUT</a>
+					</td>
+					<td id="headerbar_button">
+						<a id="headerbar_link" href="#"><i class="fa fa-photo"></i>&nbsp;GALLERY</a>
+					</td>
+					<td id="headerbar_button">
+						<a id="headerbar_link" href="#"><i class="fa fa-phone"></i>&nbsp;CONTACT US</a>
+					</td>
+					<td width="50%"></td>
+				</tr>
+			</table>
+			<table width="100%">
+			<!--slideshow  + navbar -->
+				<tr>
+					<td width="30%"></td>
+					<td  width="40%">
+						<table width="100%">
+							<form action="#" method="get">
+								
+								<tr width="100%" style>
+									<td width="100%" id="Login_header" style="padding:15;padding-left:100px ">Sign UP</td>
+								</tr>
+
+								<tr width="100%" style="background-color:#4E4E50">
+									<td id="loginform_text" style="padding:15;padding-left: 100">
+										Name
+									</td>
+									<td style="padding:15px;height:20px"> 
+										<input type="text" name="customer_name" placeholder="Enter Name" style="padding:15px;height:20px">
+									</td>
+								</tr>
+								<tr width="100%" style="background-color:#4E4E50">
+									<td id="loginform_text" style="padding:15;padding-left: 100">
+										Age
+									</td>
+									<td style="padding:15px;height:20px"> 
+										<input type="text" name="customer_age" placeholder="Enter Age" style="padding:15px;height:20px">
+									</td>
+								</tr>
+								<tr width="100%" style="background-color:#4E4E50">
+									<td id="loginform_text" style="padding:15;padding-left: 100">
+										Address
+									</td>
+									<td style="padding:15px;height:20px"> 
+										<input type="text" name="customer_address" placeholder="Enter Address" style="padding:15px;height:20px">
+									</td>
+								</tr>
+								<tr width="100%" style="background-color:#4E4E50">
+									<td id="loginform_text" style="padding:15;padding-left: 100">
+										Gender
+									</td>
+									<td id="loginform_text" style="padding:15;padding-left: 100">
+										Male<input type="checkbox" name="gender" value="male" style="padding:15px;height:20px">
+										Female<input type="checkbox" name="gender" value="female" style="padding:15px;height:20px">
+									</td>
+								</tr>
+
+								<tr width="100%" style="background-color:#4E4E50">
+									<td id="loginform_text" style="padding:15;padding-left: 100">
+										Password
+									</td>
+									<td style="padding:15">
+										<input type="password" name="password" placeholder="****" style="padding:15px;height:20px">
+									</td>
+								</tr>
+								<tr width="100%" style="background-color:#4E4E50">
+									<td id="loginform_text" style="padding:15;padding-left: 100">
+										Confirm Password
+									</td>
+									<td style="padding:15">
+										<input type="password" name="password" placeholder="****" style="padding:15px;height:20px">
+									</td>
+								</tr>
+
+								<tr width="100%">
+									<td style="padding-left: 180">
+										<input type="submit" value="submit" id="submit_button">
+									</td>
+								</tr>
+
+							</form>
+						</table>
+
+					</td>
+					<td width="30%"></td>
+					
+				</tr>
+			</table>
+			<br>
+			<!--navbar-->
+			<table width="100%">
+				<tr id="navbar" width="100%">
+					<td id="navbar_obj" width="20%">Buy Mobile</td>
+					<td width="6%"></td>
+					<td id="navbar_obj" width="20%">Request Service</td>
+					<td width="6%"></td>
+					<td id="navbar_obj" width="20%">Locate Us</td>
+					<td width="6%"></td>
+					<td id="navbar_obj" width="20%">Delivery</td>
+				</tr>
+			</table>
+			<br><br>
+			<table width="100%">
+			<!--footer-->
+				<tr width="100%">
+					<td width="100%">
+						<div id="footer" >
+							<div id="footer_button">
+								<a href="#" class="buttonf" >About  </a><div id="I_foot">|</div>
+								<a href="#" class="buttonf">Help  </a><div id="I_foot">|</div>
+								<a href="#" class="buttonf">Blog</a><div id="I_foot">|</div>
+								<a href="#" class="buttonf">Jobs</a><div id="I_foot">|</div>
+								<a href="#" class="buttonf">Terms</a><div id="I_foot">|</div>
+								<a href="#" class="buttonf">Privacy</a><div id="I_foot">|</div>
+								<a href="#" class="buttonf">Media</a><div id="I_foot">|</div>
+								<a href="#" class="buttonf">Developers</a><div id="I_foot">|</div>
+								<a href="#" class="buttonf">Directory</a>
+							</div>
+							<br><br>
+							<div id="cr" align="center">
+						   		Copyrighted@2012<br>Site designed by cse15 
+						   </div>
+						</div>
+					</td>
+					
+				</tr>
+		</table>
+	</body>
+</html>
